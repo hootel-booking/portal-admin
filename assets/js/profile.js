e0fea0e93733c7d604473d2976fecaec6cdb54e9
DESKTOP-RNK2HL9\DELL <dthiep.gm@gmail.com>
2023-10-30 23:01:34 +0700

    feature/store-and-load-image-user

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -10,83 +10,84 @@ $(document).ready(function () {
     }).done(function (data) {
         const user = data?.data;
         let htmlDisplay = "";
+        const avatar = data?.data?.avatar;
+        const dataImage = avatar ?
+            `http://localhost:8080/file/pathImage=avatars&fileName=${avatar}` :
+            "../assets/img/elements/12.jpg";
 
-        if (!!user) {
             htmlDisplay = `
-                <div class="row g-0">
-                    <div class="col-md-4">
-                        <img class="card-img card-img-left" src="../assets/img/elements/12.jpg" alt="Card image" />
-                    </div>
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title">${user.userName}</h5>
-                            <p class="card-text">
-                                <div class="col-lg-12">
-                                    <div class="card mb-4">
-                                        <div class="card-body">
-                                            <div class="row">
-                                                <div class="col-sm-3">
-                                                    <p class="mb-0">First Name</p>
-                                                </div>
-                                                <div class="col-sm-9">
-                                                    <p class="text-muted mb-0">${user.firstname}</p>
-                                                </div>
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img class="card-img card-img-left" src=${dataImage} alt="Card image" />
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">${user.userName ? user.userName : "Username"}</h5>
+                        <p class="card-text">
+                            <div class="col-lg-12">
+                                <div class="card mb-4">
+                                    <div class="card-body">
+                                        <div class="row">
+                                            <div class="col-sm-3">
+                                                <p class="mb-0">First Name</p>
                                             </div>
-                                            <hr>
-
-                                            <div class="row">
-                                                <div class="col-sm-3">
-                                                    <p class="mb-0">Last Name</p>
-                                                </div>
-                                                <div class="col-sm-9">
-                                                    <p class="text-muted mb-0">${user.lastName}</p>
-                                                </div>
+                                            <div class="col-sm-9">
+                                                <p class="text-muted mb-0">${user.firstname ? user.firstname : ""}</p>
                                             </div>
-                                            <hr>
+                                        </div>
+                                        <hr>
 
-                                            <div class="row">
-                                                <div class="col-sm-3">
-                                                    <p class="mb-0">Email</p>
-                                                </div>
-                                                <div class="col-sm-9">
-                                                    <p class="text-muted mb-0">${user.email}</p>
-                                                </div>
-                                            </div
-                                            <hr>
-                                            <hr>
-                                            
-                                            <div class="row">
-                                                <div class="col-sm-3">
-                                                    <p class="mb-0">Username</p>
-                                                </div>
-                                                <div class="col-sm-9">
-                                                    <p class="text-muted mb-0">${user.userName}</p>
-                                                </div>
+                                        <div class="row">
+                                            <div class="col-sm-3">
+                                                <p class="mb-0">Last Name</p>
                                             </div>
-                                            <hr>
+                                            <div class="col-sm-9">
+                                                <p class="text-muted mb-0">${user.lastName ? user.lastName : ""}</p>
+                                            </div>
+                                        </div>
+                                        <hr>
 
-                                            <div class="row">
-                                                <div class="col-sm-3">
-                                                    <p class="mb-0">Phone</p>
-                                                </div>
-                                                <div class="col-sm-9">
-                                                    <p class="text-muted mb-0">${user.phone}</p>
-                                                </div>
+                                        <div class="row">
+                                            <div class="col-sm-3">
+                                                <p class="mb-0">Email</p>
+                                            </div>
+                                            <div class="col-sm-9">
+                                                <p class="text-muted mb-0">${user.email ? user.email : ""}</p>
+                                            </div>
+                                        </div
+                                        <hr>
+                                        <hr>
+                                        
+                                        <div class="row">
+                                            <div class="col-sm-3">
+                                                <p class="mb-0">Username</p>
+                                            </div>
+                                            <div class="col-sm-9">
+                                                <p class="text-muted mb-0">${user.userName ? user.userName : ""}</p>
+                                            </div>
+                                        </div>
+                                        <hr>
+
+                                        <div class="row">
+                                            <div class="col-sm-3">
+                                                <p class="mb-0">Phone</p>
+                                            </div>
+                                            <div class="col-sm-9">
+                                                <p class="text-muted mb-0">${user.phone ? user.phone : ""}</p>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                            </p>
-                            <div class="mt-2">
-                                <button type="reset" class="btn btn-outline-secondary btn-back">Back</button>
                             </div>
+                        </p>
+                        <div class="mt-2">
+                            <button type="reset" class="btn btn-outline-secondary btn-back">Back</button>
                         </div>
                     </div>
                 </div>
-            `;
-
-            idElForm.innerHTML = htmlDisplay;
-        }
+            </div>
+        `;
+        idElForm.innerHTML = htmlDisplay;
     }).fail(function(jqXHR, textStatus, errorThrown) {
         alert("Error " + errorThrown);
     });
